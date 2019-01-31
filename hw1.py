9910130465d3f79391962cb9e3e58297688cda35
David Silverman <oneski@users.noreply.github.com>
2019-01-31 15:33:08 -0500

    initial commit

---
+++ hw1.py
@@ -0,0 +1,18 @@
+from skimage import io 
+from skimage.transform import resize
+import numpy as np
+
+def task_1():
+    pass
+
+def task_2():
+    pass
+
+def task_3():
+    pass
+
+if __name__ == "__main__":
+    task_1()
+    task_2()
+    task_3()
+
